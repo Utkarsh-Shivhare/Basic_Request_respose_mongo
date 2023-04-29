51f071da92195246a7e418f6f45a50baff647a10
Utkarsh-Shivhare <109977467+Utkarsh-Shivhare@users.noreply.github.com>
2023-04-29 19:21:36 +0530

    Delete mongo_extraction.py

--- mongo_extraction.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-client=pymongo.MongoClient("mongodb://localhost:27017")
-db=client["Government"] # Database Name
-collec=db["Data"]
-data=collec.find_one()
-print(type(data))
-print(data)
